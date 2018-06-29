0881b525b4d95a355d4d78f1d73123d03fc71dc4
msgirlperl <mspeert@gmail.com>
2018-06-28 21:13:52 -0400

    more tests and stubbed out updateAnalyticsData

--- server/scripts/updateAnalyticsData.js
+++ server/scripts/updateAnalyticsData.js
@@ -1,6 +1,8 @@
-const path = require('path')
-const low = require('lowdb')
-const FileSync = require('lowdb/adapters/FileSync')
+const path = require('path');
+const low = require('lowdb');
+const FileSync = require('lowdb/adapters/FileSync');
 
-const adapter = new FileSync(path.join(__dirname, '../database/db.json'))
-const db = low(adapter)
+const adapter = new FileSync(path.join(__dirname, '../database/db.json'));
+const db = low(adapter);
+
+module.exports.fetchLatestTweetsIntoDB = () => {};
